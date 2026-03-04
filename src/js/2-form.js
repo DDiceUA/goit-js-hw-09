46b6d53f8d4c0dd98f803488137932d594cf10b6
Olexandr Usarov <skeler228@gmail.com>
2026-03-04 19:14:55 +0200

    first commit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,33 @@
-console.log(2);
+const formData = {
+    message : '',
+    email: '',
+}
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input',e=>{
+    const fD = new FormData(form);
+    formData.message = fD.get('message');
+    formData.email = fD.get('email');
+    localStorage.setItem("feedback-form-state",JSON.stringify(formData));
+})
+
+if(localStorage.getItem("feedback-form-state")){
+    const loadedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    formData.email = loadedData.email;
+    formData.message = loadedData.message;
+    form.email.value = formData.email;
+    form.message.value = formData.message;
+}
+
+form.addEventListener('submit',e=> {
+    e.preventDefault();
+    if (form.email.value && form.message.value){
+        console.log(formData);
+        formData.email = '';
+        formData.message = '';
+        localStorage.removeItem('feedback-form-state');
+    }else{
+        alert('Fill please all fields');
+        return;
+    }
+    form.reset()
+})
